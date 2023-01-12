8561923d3e6d71cd193c1184b7805c2ba6cedd8f
GHGHGHKO <gudrb963@gmail.com>
2023-01-12 22:27:48 +0900

    refactor: common entity separation

---
+++ src/main/kotlin/com/example/pepega/helloworld/domain/CommonEntity.kt
@@ -0,0 +1,45 @@
+package com.example.pepega.helloworld.domain
+
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.PostLoad
+import jakarta.persistence.PostPersist
+import org.springframework.data.domain.Persistable
+import java.util.Objects
+import java.util.UUID
+
+@MappedSuperclass
+class CommonEntity(
+
+) : Persistable<UUID> {
+
+    @Id
+    private val id: UUID = UUID.randomUUID()
+
+    @Transient
+    private var _isNew = true
+
+    override fun getId(): UUID = id
+
+    override fun isNew(): Boolean = _isNew
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) {
+            return false
+        }
+
+        if (this::class != other::class) {
+            return false
+        }
+
+        return id == (other as HelloWorld).id
+    }
+
+    override fun hashCode() = Objects.hashCode(id)
+
+    @PostPersist
+    @PostLoad
+    protected fun load() {
+        _isNew = false
+    }
+}
--- src/main/kotlin/com/example/pepega/helloworld/domain/HelloWorld.kt
+++ src/main/kotlin/com/example/pepega/helloworld/domain/HelloWorld.kt
@@ -2,51 +2,15 @@ package com.example.pepega.helloworld.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.PostLoad
-import jakarta.persistence.PostPersist
 import jakarta.persistence.Table
-import org.springframework.data.domain.Persistable
-import java.util.Objects
-import java.util.UUID
 
 @Entity
 @Table(name = "Foo")
 class HelloWorld (
     @Column
     val name: String
-) : Persistable<UUID> {
-
-    @Id
-    private val id: UUID = UUID.randomUUID()
+) : CommonEntity() {
 
     @Column
     var age: Int = 0
-
-    @Transient
-    private var _isNew = true
-
-    override fun getId(): UUID = id
-
-    override fun isNew(): Boolean = _isNew
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null) {
-            return false
-        }
-
-        if (this::class != other::class) {
-            return false
-        }
-
-        return id == (other as HelloWorld).id
-    }
-
-    override fun hashCode() = Objects.hashCode(id)
-
-    @PostPersist
-    @PostLoad
-    protected fun load() {
-        _isNew = false
-    }
 }
