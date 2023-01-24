2f6ae1d6af5ade6e8bdaf332c01f408562656087
GHGHGHKO <gudrb963@gmail.com>
2023-01-24 16:45:48 +0900

    feat: add BCryptPasswordEncoder test code

--- src/main/kotlin/com/example/pepega/common/service/sign/SignService.kt
+++ src/main/kotlin/com/example/pepega/common/service/sign/SignService.kt
@@ -4,11 +4,13 @@ import com.example.pepega.common.advice.UserExistExceptionCustom
 import com.example.pepega.common.domain.UserMaster
 import com.example.pepega.common.dto.sign.SignUpRequestDto
 import com.example.pepega.common.repository.UserMasterRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class SignService(
-    private val userMasterRepository: UserMasterRepository
+    private val userMasterRepository: UserMasterRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
     fun signUp(signUpRequestDto: SignUpRequestDto) {
         val user = userMasterRepository.findByEmail(signUpRequestDto.email)
@@ -23,7 +25,7 @@ class SignService(
         userMasterRepository.save(
             UserMaster(
                 email = signUpRequestDto.email,
-                password = signUpRequestDto.password,
+                password = passwordEncoder.encode(signUpRequestDto.password),
                 nickName = signUpRequestDto.nickname,
                 createUser = SIGN_UP_SERVICE_NAME,
                 updateUser = SIGN_UP_SERVICE_NAME,
---
+++ src/test/kotlin/com/example/pepega/common/service/sign/SignServiceTest.kt
@@ -0,0 +1,48 @@
+package com.example.pepega.common.service.sign
+
+import com.example.pepega.common.dto.sign.SignUpRequestDto
+import com.example.pepega.common.repository.UserMasterRepository
+import org.junit.jupiter.api.Assertions.assertNotEquals
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class SignServiceTest(
+    @Autowired
+    val signService: SignService,
+
+    @Autowired
+    val userMasterRepository: UserMasterRepository
+) {
+
+    @Test
+    fun `패스워드_암호화_여부`() {
+
+        //given
+        val signUpRequestDto = SignUpRequestDto(
+            email = "pepega@pepetheflog.com",
+            password = "3q4mf9ao8eirghj",
+            nickname = "flog"
+        )
+
+        //when
+        signService.signUp(signUpRequestDto)
+
+        //then
+        val findSignUpUser = userMasterRepository
+            .findByEmail(signUpRequestDto.email)
+
+        if (findSignUpUser != null) {
+            assertNotEquals(signUpRequestDto.password, findSignUpUser.password)
+
+            println("Input password : ${signUpRequestDto.password}, " +
+                    "Output password : ${findSignUpUser.password}")
+        }
+    }
+}
