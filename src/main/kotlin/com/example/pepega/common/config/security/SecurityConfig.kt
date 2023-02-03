316e4a8fbecf3e61b9c791ca2b2b8d013de7ac0a
GHGHGHKO <gudrb963@gmail.com>
2023-02-03 18:55:20 +0900

    feat: add JwtAuthenticationFilter in SecurityConfig

--- src/main/kotlin/com/example/pepega/common/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/pepega/common/config/security/SecurityConfig.kt
@@ -1,13 +1,17 @@
 package com.example.pepega.common.config.security
 
+import com.example.pepega.common.config.security.filter.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig (
+    private val jwtTokenProvider: JwtTokenProvider
+) {
 
     @Bean
     fun filterChain(httpSecurity: HttpSecurity
@@ -20,6 +24,10 @@ class SecurityConfig {
             .and()
             .authorizeHttpRequests()
             .requestMatchers("/sign/*/signIn", "/sign/*/signUp").permitAll()
+            .and()
+            .addFilterBefore(
+                JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter::class.java)
 
         return httpSecurity.build()
     }
