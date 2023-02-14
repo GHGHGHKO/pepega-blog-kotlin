e1cfbd9b5631aaf8d5d27b26282de1f0cfce755d
GHGHGHKO <gudrb963@gmail.com>
2023-02-14 22:24:01 +0900

    feat: success actuator test code

---
+++ src/test/kotlin/com/example/pepega/common/controller/ActuatorControllerTest.kt
@@ -0,0 +1,62 @@
+package com.example.pepega.common.controller
+
+import com.example.pepega.common.dto.sign.request.SignInRequestDto
+import com.example.pepega.common.dto.sign.request.SignUpRequestDto
+import com.example.pepega.common.service.sign.SignService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class ActuatorControllerTest(
+    @Autowired
+    val mockMvc: MockMvc,
+
+    @Autowired
+    val signService: SignService
+) {
+
+    companion object {
+        private const val EXIST_EMAIL = "existUser@real.com"
+        private const val EXIST_PASSWORD = "existPassword!@#s2"
+        private const val EXIST_NICKNAME = "patric"
+        private const val X_AUTH_TOKEN = "X-AUTH-TOKEN"
+    }
+
+    lateinit var token: String
+
+    @BeforeEach
+    fun setUp() {
+        val signUpRequestDto = SignUpRequestDto(
+            email = EXIST_EMAIL,
+            password = EXIST_PASSWORD,
+            nickname = EXIST_NICKNAME
+        )
+
+        val signInRequestDto = SignInRequestDto(
+            email = EXIST_EMAIL,
+            password = EXIST_PASSWORD
+        )
+
+        signService.signUp(signUpRequestDto)
+        token = signService.signIn(signInRequestDto).token
+    }
+
+    @Test
+    fun `actuator 호출 성공한다`() {
+
+        mockMvc.get("/actuator") {
+            header(X_AUTH_TOKEN, token)
+        }
+            .andDo { print() }
+            .andExpect { status { isOk() } }
+            .andExpect { jsonPath("$._links") { exists() } }
+    }
+}
